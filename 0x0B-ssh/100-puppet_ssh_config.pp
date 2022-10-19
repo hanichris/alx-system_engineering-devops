cd4517548e2c3467638bed93f165bb9ccea77d5b
hanichris <hanichris71@gmail.com>
2022-10-18 17:39:20 -0700

    Using file_line instead of file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,14 @@
 #Using `Puppet`resource `file_line` to make changes to a configuration file
-file { '/root/.ssh/config':
-  ensure  => present,
-  content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no",
+file_line { 'Define the Identity file to use':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
 }
+
+file_line { 'Disable password authentication':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+include stdlib
