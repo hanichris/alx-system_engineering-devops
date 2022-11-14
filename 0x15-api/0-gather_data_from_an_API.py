ae4d384cde0abd08283ca1612c022ced3c7f9239
hanichris <hanichris71@gmail.com>
2022-11-14 22:43:06 +0000

    Python Script to query a REST API. Display the employee TODO list progress in a formatted manner.

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Gather data from a REST API."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    todo_url = "https://jsonplaceholder.typicode.com/todos/"
+    emp_id = sys.argv[1]
+
+    response = requests.get('%s%s' % (user_url, emp_id)).json()
+    name = response['name']
+    user_id = response['id']
+
+    res = requests.get('%s?userId=%s' % (todo_url, user_id)).json()
+    completed = list(filter(lambda x: (x['completed'] == True), res))
+    
+    total_tasks = len(res)
+    completed_tasks = len(completed)
+
+    first_line = 'Employee %s is done with tasks(%d/%d):' % (name,
+            completed_tasks, total_tasks)
+    print(first_line)
+    for task in completed:
+        print('\t %s' % (task['title']))
