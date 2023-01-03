609658a3abbb16f26bc1515ed85a83cf9f0a2c87
root <hanichris71@gmail.com>
2023-01-03 09:25:14 -0800

    bug fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,7 +8,7 @@ from collections import Counter
 import requests
 
 
-def _util_count_words(subreddit, word_list, hot_list=[], after=''):
+def count_words(subreddit, word_list, hot_list=[], after=''):
     """Return a list containing the titles of all the hot posts.
 
     Args:
@@ -27,6 +27,8 @@ def _util_count_words(subreddit, word_list, hot_list=[], after=''):
     header = {'user-agent': 'python_script'}
     r = requests.get(url, params=payload, headers=header,
                      allow_redirects=False)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
     if r.status_code == 404:
         return None
     r = r.json().get('data')
@@ -38,35 +40,19 @@ def _util_count_words(subreddit, word_list, hot_list=[], after=''):
         if len(_x) > 0:
             hot_list.extend(_x)
     if after:
-        return _util_count_words(subreddit, word_list, hot_list, after)
+        return count_words(subreddit, word_list, hot_list, after)
     else:
-        return hot_list
-
-
-def count_words(subreddit, word_list):
-    """Print sorted count of given keywords.
-
-    Queries the Reddit API and parses the title of all hot articles
-    to generate a list of them before processing it to generate
-    a dictionary of the given keywords.
-    Args:
-        subreddit (str): subreddit to be queried.
-        word_list (list): list of keywords to search for.
-    """
-    word_list = [word.lower() for word in word_list]
-    hot_list = _util_count_words(subreddit, word_list)
-    if hot_list is None:
-        return None
-    result = {}
-    for word in hot_list:
-        if word not in result:
-            result[word] = 1
-        elif word in result:
-            result[word] += 1
-    count = Counter(word_list)
-    for key, value in count.items():
-        if value > 1:
-            result[key] *= value
-    for key, value in sorted(result.items(),
-                             key=lambda x: x[1], reverse=True):
-        print('{}: {}'.format(key, value))
+        word_list = [word.lower() for word in word_list]
+        result = {}
+        for word in hot_list:
+            if word not in result:
+                result[word] = 1
+            elif word in result:
+                result[word] += 1
+        count = Counter(word_list)
+        for key, value in count.items():
+            if value > 1:
+                result[key] *= value
+        for key, value in sorted(result.items(),
+                                 key=lambda x: (x[1], x[0]), reverse=True):
+            print('{}: {}'.format(key, value))
