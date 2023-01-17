14bd4b57008a9dc78b7b194de942c1d88f286fb3
root <hanichris71@gmail.com>
2023-01-17 14:34:32 -0800

    Puppet manifest to configure an ubuntu machine

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,24 @@
 # HTTP header response, name X-Served-By, value of the custom HTTP header must be the hostname of the server Nginx is running on
+
 exec { 'update':
-  command  => 'sudo apt-get update',
+  command  => 'sudo apt-get -y update',
   provider => shell,
 }
--> package {'nginx':
+
+package {'nginx':
   ensure => present,
+  name   => 'nginx',
 }
--> file_line { 'header line':
+
+file_line { 'header line':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "	location / {
-  add_header X-Served-By ${hostname};",
+  add_header X-Served-By ${HOSTNAME};",
   match  => '^\tlocation / {',
 }
--> exec { 'restart service':
+
+exec { 'restart service':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
